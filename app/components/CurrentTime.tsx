f0a8300cc0c2703bc05a120e9489284c241106c6
yuma.wada <yuma.wada.1127@gmail.com>
2024-04-13 00:11:37 +0900

    Fix CurrentTime style

--- app/components/CurrentTime.tsx
+++ app/components/CurrentTime.tsx
@@ -1,13 +1,31 @@
 import { useEffect, useState } from "react";
 
+const localeOptions = {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+} as const;
+
+const timeStyle: React.CSSProperties = {
+  fontSize: "4rem",
+  color: "gray",
+  background: "#c1c1c1",
+  height: "25rem",
+  width: "25rem",
+  borderRadius: "50%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
 export function CurrentTime() {
   const [time, setTime] = useState<null | string>(null);
 
   useEffect(() => {
-    setTime(new Date().toLocaleTimeString());
+    setTime(new Date().toLocaleTimeString([], localeOptions));
 
     const timerId = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
+      setTime(new Date().toLocaleTimeString([], localeOptions));
     }, 100);
 
     // コンポーネントのアンマウント時にタイマーをクリア
@@ -18,5 +36,5 @@ export function CurrentTime() {
   // ref: https://nextjs.org/docs/messages/react-hydration-error
   if (!time) return <div>Loading...</div>;
 
-  return <time>{time}</time>;
+  return <div style={timeStyle}>{time}</div>;
 }
