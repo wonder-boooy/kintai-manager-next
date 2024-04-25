fbefd0633a9b68095f7888c9ef7243567f7c8fcc
yuma.wada <yuma.wada.1127@gmail.com>
2024-04-25 23:16:25 +0900

    Enable generation of break records

--- app/components/BreakButton.tsx
+++ app/components/BreakButton.tsx
@@ -1,15 +1,48 @@
 import { Button } from "@/shared/components/Button";
 import Flex from "@/shared/components/Flex";
-import { FaPause } from "react-icons/fa6";
+import { FaArrowRotateRight, FaPause } from "react-icons/fa6";
 import { db } from "@/shared/utils/db";
 import { useLiveQuery } from "dexie-react-hooks";
 
-export function BreakButton() {
-  const lastWork = useLiveQuery(() => db.works.toArray())?.slice(-1)[0];
-  const stillWorking = !!lastWork && !lastWork.finishedAt;
+function PauseButton() {
+  return (
+    <Flex>
+      <FaPause size={30} />
+      休憩
+    </Flex>
+  );
+}
 
-  const takeBreak = () => {
+function RestartButton() {
+  return (
+    <Flex>
+      <FaArrowRotateRight size={30} />
+      再開
+    </Flex>
+  );
+}
+
+export function BreakButton() {
+  const lastWork = useLiveQuery(async () => await db.works.toArray())?.slice(
+    -1
+  )[0];
+  const breakRecord = useLiveQuery(
+    () => db.breaks.where({ workId: lastWork?.id || 0 }).toArray(),
+    [lastWork]
+  )?.slice(-1)[0];
+  const stillWorking = !!lastWork && !lastWork.finishedAt;
+  const stillBreaking =
+    stillWorking && !!breakRecord && !breakRecord.finishedAt;
+
+  const toggleBreak = () => {
     if (!lastWork) return;
+
+    if (stillBreaking) {
+      db.breaks.update(breakRecord.id as number, {
+        finishedAt: new Date(),
+      });
+      return;
+    }
 
     db.breaks.add({
       startedAt: new Date(),
@@ -18,11 +51,8 @@ export function BreakButton() {
   };
 
   return (
-    <Button onClick={takeBreak} disabled={!stillWorking}>
-      <Flex>
-        <FaPause size={30} />
-        休憩
-      </Flex>
+    <Button onClick={toggleBreak} disabled={!stillWorking}>
+      {stillBreaking ? <RestartButton /> : <PauseButton />}
     </Button>
   );
 }
