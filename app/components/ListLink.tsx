5e880898995ee2792943f6a23908c054124c5861
yuma.wada <yuma.wada.1127@gmail.com>
2024-04-26 10:14:51 +0900

    Add a link component to access lists

---
+++ app/components/ListLink.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/shared/components/Button";
+import Flex from "@/shared/components/Flex";
+import Link from "next/link";
+import { FaCircleArrowRight } from "react-icons/fa6";
+
+const listLinkStyle = {
+  color: "#fff",
+  fontSize: 18,
+  background: "rgba(255,255,255,0.2)",
+} as const;
+
+export function ListLink() {
+  return (
+    <Link href="/lists">
+      <Button disabled={false} style={listLinkStyle}>
+        <Flex align="center">
+          一覧
+          <FaCircleArrowRight />
+        </Flex>
+      </Button>
+    </Link>
+  );
+}
