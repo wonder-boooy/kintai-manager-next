6b035b62699f87844a8468cb4a8b6e37ac5b15fa
yuma.wada <yuma.wada.1127@gmail.com>
2024-04-30 11:12:59 +0900

    Add japanese labels

--- shared/components/LapTimes.tsx
+++ shared/components/LapTimes.tsx
@@ -2,13 +2,20 @@ import Container from "./Container";
 import { Absolute } from "./Absolute";
 import { WorkingTime } from "./WorkingTime";
 import { BreakingTime } from "./BreakingTime";
+import Flex from "./Flex";
 
 export function LapTimes() {
   return (
     <Absolute top={25} left={25}>
       <Container>
-        <WorkingTime />
-        <BreakingTime />
+        <Flex>
+          実働
+          <WorkingTime />
+        </Flex>
+        <Flex>
+          休憩
+          <BreakingTime />
+        </Flex>
       </Container>
     </Absolute>
   );
