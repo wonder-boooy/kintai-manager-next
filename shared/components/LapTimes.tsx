a21506d25113ed269f5bcc2d695f4190571a8ded
yuma.wada <yuma.wada.1127@gmail.com>
2024-05-01 14:43:59 +0900

    Don't display unnecessary components

--- shared/components/LapTimes.tsx
+++ shared/components/LapTimes.tsx
@@ -3,19 +3,38 @@ import { Absolute } from "./Absolute";
 import { WorkingTime } from "./WorkingTime";
 import { BreakingTime } from "./BreakingTime";
 import Flex from "./Flex";
+import { useWork } from "../hooks/useWork";
+import { useBreak } from "../hooks/useBreak";
+
+function WorkLap() {
+  return (
+    <Flex>
+      実働
+      <WorkingTime />
+    </Flex>
+  );
+}
+
+function BreakLap() {
+  return (
+    <Flex>
+      休憩
+      <BreakingTime />
+    </Flex>
+  );
+}
 
 export function LapTimes() {
+  const { lastWork, stillWorking } = useWork();
+  const { breakRecord, stillBreaking } = useBreak(lastWork);
+
+  if (!stillWorking) return null;
+
   return (
     <Absolute top={25} left={25}>
       <Container>
-        <Flex>
-          実働
-          <WorkingTime />
-        </Flex>
-        <Flex>
-          休憩
-          <BreakingTime />
-        </Flex>
+        {stillWorking && <WorkLap />}
+        {breakRecord && <BreakLap />}
       </Container>
     </Absolute>
   );
