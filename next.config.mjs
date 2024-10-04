13cf258d0ec10d3489e2091868ff385800eef945
yuma.wada <yuma.wada.1127@gmail.com>
2024-10-04 23:51:41 +0900

    Make this app PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  scope: "/",
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({});
 
 export default nextConfig;
